c88c8787da7eade76637be1528ade45f1e6c20ce
Ahmed Teleb <ahmedasd37@gmail.com>
2021-06-20 04:09:46 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $connection=new PDO('mysql:host=localhost;dbname=vendor;','root','');
+    $connection=new PDO('mysql:host=sql11.freemysqlhosting.net;dbname=sql11420188;','sql11420188','Q8GH7rDKSE');
 
 }catch (PDOException $exception){
     echo $exception->getMessage();
